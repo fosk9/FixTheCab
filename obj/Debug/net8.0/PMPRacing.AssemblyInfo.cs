119fcf81e5b4427129f3464ef69714807bc02c8c
f0sk9 <169178444+f0sk9@users.noreply.github.com>
2026-03-18 22:58:29 +0700

    Can sumit and pay simulated

--- obj/Debug/net8.0/PMPRacing.AssemblyInfo.cs
+++ obj/Debug/net8.0/PMPRacing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PMPRacing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ad54c65d200dcf74dd6241ca2c32151632254d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c6c69aec632c39667f0f4e893b6198bf20e3e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PMPRacing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PMPRacing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
